1b59cdc9def6eeaff692dc967c32f22e2dca5d70
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-05 12:09:13 +0100

    snack 2

--- snack 1 es6/js/script.js
+++ snack 1 es6/js/script.js
@@ -6,3 +6,78 @@
    Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
    Infine stampa separatamente i 3 array.
  */
+
+
+   const cars = [
+    {
+        marca : 'mercedes',
+        modello : 'cls 6.3',
+        alimentazione : 'benzina'
+    },
+    {
+        marca : 'bmw',
+        modello : 'm2',
+        alimentazione : 'diesel'
+    },
+    {
+        marca : 'tesla',
+        modello : 'plaid',
+        alimentazione : 'elettrico'
+    },
+    {
+        marca : 'mercedes',
+        modello : 'c coupe 300',
+        alimentazione : 'elettrico'
+    },
+    {
+        marca : 'fiat',
+        modello : '500',
+        alimentazione : 'benzina'
+    },
+    {
+        marca : 'mercedes',
+        modello : 'classe A',
+        alimentazione : 'diesel'
+    },
+    {
+        marca : 'maserati',
+        modello : 'ghibli',
+        alimentazione : 'benzina'
+    },
+    {
+        marca : 'lamborghini',
+        modello : 'urus',
+        alimentazione : 'benzina'
+    },
+    {
+        marca : 'porche',
+        modello : '911',
+        alimentazione : 'benzina'
+    },
+    {
+        marca : 'mercedes',
+        modello : 'G 500',
+        alimentazione : 'benzina'
+    },
+]
+
+const benzina = [];
+const disel = [];
+const other = []; 
+
+for(let i = 0; i < cars.length; i++){
+    for(key in cars[i]){
+        if(cars[i].alimentazione == 'benzina'){
+            benzina.push(cars[i])
+        }else if(cars[i].alimentazione == 'disel'){
+            disel.push(cars[i])
+        }else{
+            other.push(cars[i])
+        }
+    }
+}
+
+console.log(benzina);
+console.log(disel);
+console.log(other);
+
